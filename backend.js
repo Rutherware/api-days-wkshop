a09a0a2f6a6f6439580c6e32429b22c1bc6859f4
Rutherford <68884881+Rutherware@users.noreply.github.com>
2020-07-28 06:00:03 -0400

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = 3010;
+const request = require('request');
+const truliooApiKey = ""; // YOUR API KEY
+const truliooMiddleware = require('trulioo-embedid-middleware')({
+  apiKey: truliooApiKey
+});
+
+app.use(truliooMiddleware);
+app.listen(port, () => console.log(`Example app listening on port!`));
